3d56384f412ba7cf1dea88226cde42f1e6f23006
Igorek9g <ifkf@mail.ru>
2017-05-12 23:47:50 +0300

    Add files via upload
    debug (needed?)

---
+++ cmake-build-debug/CMakeFiles/M_DGW_4.dir/DependInfo.cmake
@@ -0,0 +1,47 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/student/Projects/M_DGW_4-2/main.cc" "/home/student/Projects/M_DGW_4-2/cmake-build-debug/CMakeFiles/M_DGW_4.dir/main.cc.o"
+  "/home/student/Projects/M_DGW_4-2/src/ActionInit.cc" "/home/student/Projects/M_DGW_4-2/cmake-build-debug/CMakeFiles/M_DGW_4.dir/src/ActionInit.cc.o"
+  "/home/student/Projects/M_DGW_4-2/src/DetGeometry.cc" "/home/student/Projects/M_DGW_4-2/cmake-build-debug/CMakeFiles/M_DGW_4.dir/src/DetGeometry.cc.o"
+  "/home/student/Projects/M_DGW_4-2/src/EventAct.cc" "/home/student/Projects/M_DGW_4-2/cmake-build-debug/CMakeFiles/M_DGW_4.dir/src/EventAct.cc.o"
+  "/home/student/Projects/M_DGW_4-2/src/PrimaryGen.cc" "/home/student/Projects/M_DGW_4-2/cmake-build-debug/CMakeFiles/M_DGW_4.dir/src/PrimaryGen.cc.o"
+  "/home/student/Projects/M_DGW_4-2/src/RunAct.cc" "/home/student/Projects/M_DGW_4-2/cmake-build-debug/CMakeFiles/M_DGW_4.dir/src/RunAct.cc.o"
+  "/home/student/Projects/M_DGW_4-2/src/StepAction.cc" "/home/student/Projects/M_DGW_4-2/cmake-build-debug/CMakeFiles/M_DGW_4.dir/src/StepAction.cc.o"
+  "/home/student/Projects/M_DGW_4-2/src/loader.cc" "/home/student/Projects/M_DGW_4-2/cmake-build-debug/CMakeFiles/M_DGW_4.dir/src/loader.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4MULTITHREADED"
+  "G4UI_USE"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4_STORE_TRAJECTORY"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/student/geant4/geant4-install/include/Geant4"
+  "/usr/include/qt4"
+  "/usr/include/qt4/QtCore"
+  "/usr/include/qt4/QtGui"
+  "/usr/include/qt4/QtOpenGL"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/M_DGW_4.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/M_DGW_4.dir/main.cc.o"
+  "CMakeFiles/M_DGW_4.dir/src/ActionInit.cc.o"
+  "CMakeFiles/M_DGW_4.dir/src/DetGeometry.cc.o"
+  "CMakeFiles/M_DGW_4.dir/src/EventAct.cc.o"
+  "CMakeFiles/M_DGW_4.dir/src/PrimaryGen.cc.o"
+  "CMakeFiles/M_DGW_4.dir/src/RunAct.cc.o"
+  "CMakeFiles/M_DGW_4.dir/src/StepAction.cc.o"
+  "CMakeFiles/M_DGW_4.dir/src/loader.cc.o"
+  "M_DGW_4.pdb"
+  "M_DGW_4"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/M_DGW_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
